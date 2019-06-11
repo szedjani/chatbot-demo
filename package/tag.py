833753fac1df89c5f5f73b6b295a942794a8e7fe
janos.szedelenyi <janos.szedelenyi@sol-ra.com>
2019-06-12 00:06:55 +0200

    building pip module

---
+++ package/tag.py
@@ -0,0 +1,15 @@
+from attr import dataclass
+
+@dataclass
+class Tag:
+    id: int
+    tag_category: str
+    tag: str
+
+TAGS= {
+    "en": Tag(1, "Lang", "English"),
+    "de": Tag(2, "Lang", "German"),
+    "ltd": Tag(3, "Company Structure", "Ltd."),
+    "plc": Tag(4, "Company Structure", "plc"),
+    "shareholder": Tag(5, "", "Shareholder")
+}
